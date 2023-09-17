a54a89c1220b574de9a6085453ec0dc73e517dc8
thisisrenan <joserenandasilva08@gmail.com>
2023-09-17 13:21:09 -0300

    add endpoint get all Users

--- sureluck/users/views.py
+++ sureluck/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.views import View
+from .models import User
 # Create your views here.
+
+class UserView(View):
+
+    def get(self,request):
+        users= list(User.objects.values('id','name','email'))
+        if(len(users)>0):
+            data = {'status':200, 'message':'Usuarios encontrados','users':users}
+        else:
+            data = {'status':200, 'message':'não foi possivel encontrar nenhum usuario', 'users':users}
+        return JsonResponse(data)
+    def post(self, request):
+        pass
+    def put(self, request):
+        pass
+    def delete(self, request):
+        pass
