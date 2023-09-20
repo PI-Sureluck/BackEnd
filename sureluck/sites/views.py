2582a8754c232e1da4a47587ba49b46aa35946fb
thisisrenan <joserenandasilva08@gmail.com>
2023-09-20 07:56:19 -0300

    add models Event, Odds, SureBets and Relationship tables

--- sureluck/sites/views.py
+++ sureluck/sites/views.py
@@ -5,7 +5,7 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import Site
+from .models import *
 
 
 # Create your views here.
