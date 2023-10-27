073c7f85bc989bbeabe6c88a9d166d3e94ef52e4
thisisrenan <joserenandasilva08@gmail.com>
2023-10-27 15:04:41 -0300

    Calculos da calculadora prontos

--- sureluck/sites/Utilitarios/Calculadora.py
+++ sureluck/sites/Utilitarios/Calculadora.py
@@ -3,8 +3,18 @@
 class Calculadora:
 
     def verificarSureBets(self, OddtimeA, OddtimeB):
+            print(OddtimeA,OddtimeB)
             res = (1/OddtimeA) + (1/OddtimeB)
-            if(res <= 1):
+            print(res)
+            if(res >= 1):
                 return 0
-
             return 1
+
+    def verificarValorApostar(self, OddtimeA, OddtimeB):
+        res = (1 / OddtimeA) + (1 / OddtimeB)
+        res2 = (1 / res) * 100
+        timeA = (1 / OddtimeA) * res2
+        timeB = (1 / OddtimeB) * res2
+        print(timeA, timeB)
+        return [timeA, timeB]
+
