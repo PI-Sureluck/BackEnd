68dd4121b28e9b758ce63caf9166315863ec09ab
thisisrenan <joserenanadsilva08@gmail.com>
2023-12-07 19:02:42 -0300

    Login Concluido

--- sureluck/users/models.py
+++ sureluck/users/models.py
@@ -8,6 +8,8 @@ class User(AbstractUser):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=255)
+    premio = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
     username = None
 
     USERNAME_FIELD = 'email'
